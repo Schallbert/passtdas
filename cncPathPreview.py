7541e60302707bff2b4470dca63e376e7beea304
Schallbert <schal@mailbox.org>
2023-09-21 23:47:14 +0200

    fix a lot of errors in the script, still some to go

--- cncPathPreview.py
+++ cncPathPreview.py
@@ -13,7 +13,7 @@ def has_arc_command(line):
 
 def parse_coordinates(line):
     values = line.split(' ')
-    coordinate = [None, None, None, None, None]
+    coordinate = [0, 0, 25, 0, 0]
     for value in values:
         if value.find('X') > -1:
             coordinate[0] = float(value.strip('X '))
@@ -50,14 +50,14 @@ def get_arc_case(coordinates):
 def handle_arc_commands(previous_coordinates, line):
     coordinates = parse_coordinates(line)
     if line.find('G02') > -1:
-        print(coordinates)
-        previous_coordinates.append(coordinates[3])
-        previous_coordinates.append(coordinates[4])
-        print(previous_coordinates)
+        previous_coordinates[3] = coordinates[3]
+        previous_coordinates[4] = coordinates[4]
         end_state = get_arc_case(previous_coordinates)
         start_state = get_arc_case(coordinates)
 
     elif line.find('G03') > -1:
+        previous_coordinates[3] = coordinates[3]
+        previous_coordinates[4] = coordinates[4]
         end_state = get_arc_case(coordinates)
         start_state = get_arc_case(previous_coordinates)
     else:
@@ -65,23 +65,29 @@ def handle_arc_commands(previous_coordinates, line):
 
     # min/max calculations needed later on
     xyz = [coordinates[0], coordinates[1], coordinates[2]]
-    radius = sqrt((coordinates[0] - coordinates[3]) ** 2 + (coordinates[1] - coordinates[4]) ** 2)
-    x_plus_radius = [coordinates[3] + radius], coordinates[1], coordinates[2]
-    y_plus_radius = [coordinates[0], coordinates[4] + radius], coordinates[2]
-    x_minus_radius = [coordinates[3] - radius], coordinates[1], coordinates[2]
-    y_minus_radius = [coordinates[0], coordinates[4] - radius], coordinates[2]
+    radius = sqrt(coordinates[3] ** 2 + coordinates[4] ** 2)
+    x_plus_radius = [coordinates[3] + radius, coordinates[1], coordinates[2]]
+    y_plus_radius = [coordinates[0], coordinates[4] + radius, coordinates[2]]
+    x_minus_radius = [coordinates[3] - radius, coordinates[1], coordinates[2]]
+    y_minus_radius = [coordinates[0], coordinates[4] - radius, coordinates[2]]
     extremevalue_order = [y_minus_radius, x_minus_radius, x_plus_radius, y_plus_radius]
 
     statediff = end_state - start_state
+    print(radius)
+    print(line)
+    print(start_state)
+    print(end_state)
     # same quadrant?
     if statediff == 0:
-        if previous_coordinates[1] >= coordinates[1]:
+        if previous_coordinates[start_state] >= coordinates[start_state]:
             # near full circle, return all four possible max/min points
             statediff = 4
 
     result = []
+    print(statediff)
     # crossing one, two or three quadrants
     for i in range(statediff):
+        print(i)
         result.append(extremevalue_order[start_state + i])
 
     result.append(xyz)
@@ -101,17 +107,18 @@ def hello(file, name):
     with file as f:
         lines = f.readlines()
 
-        previous_line = []
+        previous_xy_command = []
         for line in lines:
             line = line.strip()
             if has_move_command(line):
                 data.append(parse_coordinates(line))
             if has_arc_command(line):
-                lines = handle_arc_commands(previous_line, line)
+                lines = handle_arc_commands(previous_xy_command, line)
                 if lines:
                     data.extend(lines)
             if data:
-                previous_line = data[-1]
+                if data[-1][0] != 0 and data[-1][1] != 0:
+                    previous_xy_command = data[-1]
 
     # data = list(enumerate(data))  # create enumerated list out of data
     #print(get_max_by_column(data, 1))
