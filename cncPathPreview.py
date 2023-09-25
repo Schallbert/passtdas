004991f20f5f5d6040411605c3647c6b7d58c24c
Schallbert <schal@mailbox.org>
2023-09-25 14:32:52 +0200

    add support for G92 coordinate offset.

--- cncPathPreview.py
+++ cncPathPreview.py
@@ -19,6 +19,11 @@ def is_arc(command):
         return (command.find('G02') > -1) or (command.find('G03') > -1)
     return False
 
+def is_coordinate_shift(command):
+    if command:
+        return command.find('G92') > -1
+    return False
+
 def parse_coordinates(line):
     """Reads a move command and converts it into a float value set
     @:param line: a move command
@@ -61,28 +66,33 @@ def get_arc_degrees(coordinates, arc_center, radius):
         rad = 2*pi - rad
     return round(degrees(rad), 0)
 
-def fill_coordinates(coordinates, previous_coordinates):
-    """helper that removes 'None' values from a line by filling with last known coordinate values
-    @:param coordinates: the current target vector, previous_coordinates: valid target vector from the past
+def fill_coordinates(coordinates, previous_coordinates, shift):
+    """helper that removes 'None' values from a line by filling with last known coordinate values and applies
+    coordinate shifts
+    @:param coordinates: the current target vector
+    @:param previous_coordinates: valid target vector from the past
+    @:param shift: the shifted coordinate system
     @:return the filled coordinate vector"""
     for i in range(0, 3):
         if coordinates[i] is None:
             coordinates[i] = previous_coordinates[i]
+        else:
+            coordinates[i] += shift[i]
     return coordinates
 
-def handle_linear_move(line, previous_coordinates):
+def handle_linear_move(line, previous_coordinates, shift):
     """handles a linear move command by parsing into floats, filling in 'None values'.
     @:param line: an input command, previous_coordinates: a valid target coordinate from the past
     @:return a valid coordinate"""
-    coordinates =  fill_coordinates(parse_coordinates(line), previous_coordinates)
+    coordinates =  fill_coordinates(parse_coordinates(line), previous_coordinates, shift)
     return [coordinates[0], coordinates[1], coordinates[2]]
 
-def handle_arc_move_ij(line, previous_coordinates):
+def handle_arc_move_ij(line, previous_coordinates, shift):
     """handles an arc move command by parsing into floats, filling in 'None values', finding out movement direction,
     arc radius, span, and position. It then finds the arc's extreme values and returns them.
     @:param line: an input command, previous_coordinates: a valid target coordinate from the past
-    @:return one or multiple valid coordinates depending on arc length and position"""
-    coordinates = fill_coordinates(parse_coordinates(line), previous_coordinates)
+    @:return one or multiple valid coordinates depending on arc length and position or empty list in case of an error."""
+    coordinates = fill_coordinates(parse_coordinates(line), previous_coordinates, shift)
     radius = sqrt(coordinates[3] ** 2 + coordinates[4] ** 2)
     arc_center = (previous_coordinates[0] + coordinates[3], previous_coordinates[1] + coordinates[4])
 
@@ -92,6 +102,8 @@ def handle_arc_move_ij(line, previous_coordinates):
     elif line.find('G03') > -1:
         arc_start = get_arc_degrees(previous_coordinates, arc_center, radius)
         arc_end = get_arc_degrees(coordinates, arc_center, radius)
+    else:
+        return []
 
     # min/max calculations needed later on
     xyz = [coordinates[0], coordinates[1], coordinates[2]]
@@ -115,6 +127,19 @@ def handle_arc_move_ij(line, previous_coordinates):
     result.append(xyz)
     return result
 
+
+def handle_coordinate_shift(line, shift):
+    """Checks how the coordinate system has been shifted. Returns the shift to be superpositioned onto move commands.
+    @:param line: The input command line
+    @:param shift: The current shift coordinates XYZ
+    @:return shift: the updated coordinate shift XYZ"""
+    coordinate_system_shift = parse_coordinates(line)
+    for i in range(0, 3):
+        if coordinate_system_shift[i]:
+            # coordinate shifted.
+            shift[i] -= coordinate_system_shift[i]
+    return shift
+
 def create_dataset_from_input(file):
     """Reads a G-code input file and generates coordinate sets for every move command.
     @:param file: the file that shall be read
@@ -124,14 +149,18 @@ def create_dataset_from_input(file):
         lines = f.readlines()
 
         previous_coordinates = [0, 0, 0]
+        shift = [0, 0, 0]
         for line in lines:
             line = line.strip()
             if is_move(line):
-                data.append(handle_linear_move(line, previous_coordinates))
+                data.append(handle_linear_move(line, previous_coordinates, shift))
             elif is_arc(line):
-                extreme_values = handle_arc_move_ij(line, previous_coordinates)
+                extreme_values = handle_arc_move_ij(line, previous_coordinates, shift)
                 if lines:
                     data.extend(extreme_values)
+            elif is_coordinate_shift(line):
+                shift = handle_coordinate_shift(line, shift)
+                click.echo(f'Found coordinate shift: X' + str(shift[0]) + ', Y' + str(shift[1]) + ' Z' + str(shift[2]))
             if data:
                 previous_coordinates = data[-1]
     return data
@@ -197,7 +226,8 @@ def convert_input_zsafety(zsafety):
 
 
 def getfileheader(targetfilename):
-    return ('(PathPreview by Schallbert, 2023)\n'
+    return ('(CNCPathPreview by Schallbert, 2023)\n'
+            '(Release: 0.8)\n'
             '(File output is supplied without liability.)\n'
             '(Output paths must be checked for correctness before usage.)\n\n'
             '(Project: ' + targetfilename + ')\n\n'
@@ -209,8 +239,8 @@ def get_info_coordinate(axis, coordinate):
         value = coordinate[0]
     elif axis == 'Y':
         value = coordinate[1]
-    return ('DLGMSG "PathPreview" "Go to ' + axis + ': ' + str(round(value, 3)) + '?"\n'
-          'IF [[#5398 == 1] AND [#5397 == 0]]  ; user pressed OK AND render mode is off \n'
+    return ('DLGMSG "PathPreview, go to ' + axis + ': ' + str(round(value, 3)) + '?"\n'
+          'IF [[#5398 == 1] AND [#5397 == 0]]\n'
           '    ' + get_gcode_rapidmove(coordinate) + 'ENDIF\n\n')
 def get_gcode_rapidmove(coordinate):
     return ('G00 X' +
@@ -219,7 +249,10 @@ def get_gcode_rapidmove(coordinate):
             str(round(coordinate[2], 3)) + '\n')
 
 def get_info_z_min(zmin):
-    return 'MSG "Maximum Z cutting depth: ' + str(round(zmin, 3)) + '"\n\n'
+    zmin = str(round(zmin, 3))
+    click.echo(f'Maximum Z depth of cut: ' + zmin + 'mm')
+    return 'MSG "Maximum Z cutting depth: ' + zmin + 'mm"\n\n'
+
 
 if __name__ == "__main__":
     path_preview()
--- readme.md
+++ readme.md
@@ -6,6 +6,10 @@ minimum values for its movements in `X` and `Y` direction.
 These values are then written to an output file that can be run as a `Jobfile`
 by the CNC application to check whether the CNC paths are within dimensions of the workpiece.
 
+## Features
+Detects "extreme values" of any given move command including arcs that have
+multiple possible values. Supports `G92` coordinate shift and takes them into account.
+
 ## Arguments
 CNCpathPreview only has one mandatory argument: The path of its input file.
 
@@ -20,3 +24,8 @@ It defaults to `25mm` if no or an erroneous value is given.
 ## Dialog
 Current Alpha version only supports *EdingCNC* syntax and thus uses a command called
 `DLGMSG` to ask the user whether the next extreme point shall be moved to.
+
+## Limitations
+Features to be added (soon):
+- Add `G02/G03 X Y R` syntax handling
+- Add support for other CNC software manufacturers (Beamicon?, EstlCAM?)
