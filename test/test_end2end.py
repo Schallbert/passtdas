1fdc7d07675d793c182b297580052b4d94d6c193
Schallbert <schallbert@mailbox.org>
2025-09-29 12:26:07 +0200

    fix end2end test paths, error tolerance

--- test/test_end2end.py
+++ test/test_end2end.py
@@ -30,7 +30,7 @@ class TestEnd2End(unittest.TestCase):
 
 #  ARC tests clockwise, counterclockwise, varying origin, definition per radius (r) and per offset (ij)
     def test_arcg03r_extremescorrect(self):
-        with open('arcg03r.tap', 'r') as f:
+        with open('test/arcg03r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [self.bottom, self.left, self.top, self.right]
@@ -38,7 +38,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_arcg02r_extremescorrect(self):
-        with open('arcg02r.tap', 'r') as f:
+        with open('test/arcg02r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [self.bottom, self.left, self.top, self.right]
@@ -46,7 +46,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_arcg02ij_extremescorrect(self):
-        with open('arcg02ij.tap', 'r') as f:
+        with open('test/arcg02ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [self.bottom, self.left, self.top, self.right]
@@ -54,7 +54,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect,list_to_float(result))
 
     def test_arcg03ij_extremescorrect(self):
-        with open('arcg03ij.tap', 'r') as f:
+        with open('test/arcg03ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [self.bottom, self.left, self.top, self.right]
@@ -62,23 +62,23 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect,list_to_float(result))
 
     def test_arcoffsetg03r_extremescorrect(self):
-        with open('arcoffsetg03r.tap', 'r') as f:
+        with open('test/arcoffsetg03r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
-            expect = [[-4.05, 1.3], [-9.61, 6.9], [-3.95, 12.56], [1.61, 6.96]]
+            expect = [[-4.06, 1.31], [-9.6, 6.89], [-3.94, 12.55], [1.6, 6.97]]
             result = cpp.get_extremes_text(targets, data)
             self.assertEqual(expect, list_to_float(result))
 
     def test_arcoffsetg02r_extremescorrect(self):
-        with open('arcoffsetg02r.tap', 'r') as f:
+        with open('test/arcoffsetg02r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
-            expect = [[-4.05, 1.3], [-9.61, 6.9], [-3.95, 12.56], [1.61, 6.96]]
+            expect = [[-4.06, 1.31], [-9.6, 6.89], [-3.94, 12.55], [1.6, 6.97]]
             result = cpp.get_extremes_text(targets, data)
             self.assertEqual(expect, list_to_float(result))
 
     def test_arcoffsetg03ij_extremescorrect(self):
-        with open('arcoffsetg03ij.tap', 'r') as f:
+        with open('test/arcoffsetg03ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[-4, 1.27], [-9.66, 6.93], [-4, 12.59], [1.66, 6.93]]
@@ -86,7 +86,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_arcoffsetg02ij_extremescorrect(self):
-        with open('arcoffsetg02ij.tap', 'r') as f:
+        with open('test/arcoffsetg02ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[-4, 1.27], [-9.66, 6.93], [-4, 12.59], [1.66, 6.93]]
@@ -94,7 +94,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_elipseg03r_extremescorrect(self):
-        with open('elipseg03r.tap', 'r') as f:
+        with open('test/elipseg03r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[0, -320], [-200, 0], [0, 320], [200, -0.01]]
@@ -102,7 +102,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, roundlistitems(list_to_float(result)))
 
     def test_elipseg02r_extremescorrect(self):
-        with open('elipseg02r.tap', 'r') as f:
+        with open('test/elipseg02r.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[0, -320], [-200, -0.01 ], [-0, 320], [200, -0]]
@@ -110,7 +110,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, roundlistitems(list_to_float(result)))
 
     def test_elipseg03ij_extremescorrect(self):
-        with open('elipseg03ij.tap', 'r') as f:
+        with open('test/elipseg03ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[0.02, -320], [-200, 0.02], [-0.02, 320], [200, -0.02]]
@@ -118,7 +118,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, roundlistitems(list_to_float(result)))
 
     def test_elipseg02ij_extremescorrect(self):
-        with open('elipseg02ij.tap', 'r') as f:
+        with open('test/elipseg02ij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[0.02, -320], [-200, 0.02], [-0.02, 320], [200, -0.02]]
@@ -126,7 +126,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, roundlistitems(list_to_float(result)))
 
     def test_rectangle_extremescorrect(self):
-        with open('rectangle.tap', 'r') as f:
+        with open('test/rectangle.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[0, 0], [0, 0], [50, 50], [50, 0]]
@@ -134,7 +134,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_irregularshapeij_extremescorrect(self):
-        with open('irregularshapeij.tap', 'r') as f:
+        with open('test/irregularshapeij.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[30.59, -50], [-50, -50], [-40, 50], [50, -45]]
@@ -142,7 +142,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_irregularshaper_extremescorrect(self):
-        with open('irregularshaper.tap', 'r') as f:
+        with open('test/irregularshaper.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[45, -50], [-50, -50], [-40, 50], [50, -45]]
@@ -150,7 +150,7 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_hugefilecoordinateshiftij_extremescorrect(self):
-        with open('hugefilecoordinateshift.tap', 'r') as f:
+        with open('test/hugefilecoordinateshift.tap', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             targets = ['Ymin', 'Xmin', 'Ymax', 'Xmax']
             expect = [[-79.1, -10.802], [-280.833, 533.76], [-109.93, 631.066], [10.802, 145.35]]
@@ -158,6 +158,6 @@ class TestEnd2End(unittest.TestCase):
             self.assertEqual(expect, list_to_float(result))
 
     def test_incompatiblefile_handlegracefully(self):
-        with open('misformulatedfile.txt', 'r') as f:
+        with open('test/misformulatedfile.txt', 'r', errors='replace') as f:
             data = cpp.create_dataset_from_input(f)
             self.assertEqual([], data)
